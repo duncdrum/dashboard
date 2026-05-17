851629b88b3e505d9130f72193a272165dd192ab
duncdrum <d.paterson@me.com>
2026-05-17 20:28:33 +0200

    refactor: upgrade gulpfile to ES modules and Gulp 5 syntax
    - Convert CommonJS require to ES module imports
    - Replace array-based task dependencies with gulp.series/parallel
    - Replace del package with native fs.promises.rm
    - Fix watch task syntax for Gulp 5
    - Update variable declarations to const

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,73 @@
 'use strict';
 
-var gulp = require('gulp'),
-    exist = require('gulp-exist'),
-    watch = require('gulp-watch'),
-    less = require('gulp-less'),
-    del = require('del'),
-    path = require('path'),
-    LessAutoprefix = require('less-plugin-autoprefix')
+import gulp from 'gulp';
+import exist from '@existdb/gulp-exist';
+import less from 'gulp-less';
+import LessAutoprefix from 'less-plugin-autoprefix';
+import { promises as fs } from 'fs';
 
-var PRODUCTION = (!!process.env.NODE_ENV || process.env.NODE_ENV === 'production')
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
-console.log('Production? %s', PRODUCTION)
+console.log('Production? %s', PRODUCTION);
 
 exist.defineMimeTypes({
     'application/xml': ['odd']
-})
+});
 
-var exClient = exist.createClient({
+const exClient = exist.createClient({
     host: 'localhost',
     port: '8080',
     path: '/exist/xmlrpc',
     basic_auth: {user: 'admin', pass: ''}
-})
+});
 
-var html5TargetConfiguration = {
+const html5TargetConfiguration = {
     target: '/db/apps/existdb-dashboard',
     html5AsBinary: true
-}
+};
 
-var targetConfiguration = {
+const targetConfiguration = {
     target: '/db/apps/existdb-dashboard'
-}
+};
 
-gulp.task('clean', function () {
-    return del(['build/**/*']);
+gulp.task('clean', async function () {
+    await fs.rm('build', { recursive: true, force: true });
 });
 
 // styles //
 
-var lessPath = './resources/css/style.less'
-var stylesPath = 'resources/css/*'
-var autoprefix = new LessAutoprefix({browsers: ['last 2 versions']})
+const lessPath = './resources/css/style.less';
+const autoprefix = new LessAutoprefix({browsers: ['last 2 versions']});
 
 gulp.task('styles', function () {
     return gulp.src(lessPath)
         .pipe(less({plugins: [autoprefix]}))
-        .pipe(gulp.dest('./resources/css'))
-})
+        .pipe(gulp.dest('./resources/css'));
+});
 
-gulp.task('deploy:styles', ['styles'], function () {
+gulp.task('deploy:styles', gulp.series('styles', function () {
     return gulp.src('resources/css/*.css', {base: './'})
         .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-})
+        .pipe(exClient.dest(targetConfiguration));
+}));
 
 // odd files //
 
-var oddPath = 'resources/odd/**/*';
+const oddPath = 'resources/odd/**/*';
+
 gulp.task('odd:deploy', function () {
     return gulp.src(oddPath, {base: './'})
         .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-})
+        .pipe(exClient.dest(targetConfiguration));
+});
 
 gulp.task('odd:watch', function () {
-    gulp.watch(oddPath, ['odd:deploy'])
-})
+    gulp.watch(oddPath, gulp.series('odd:deploy'));
+});
 
 // files in project root //
 
-var componentPaths = [
+const componentPaths = [
     '*.html',
     '!index.html',
     'bower_components/**/*'
@@ -78,10 +76,10 @@ var componentPaths = [
 gulp.task('deploy:components', function () {
     return gulp.src(componentPaths, {base: './'})
         .pipe(exClient.newer(html5TargetConfiguration))
-        .pipe(exClient.dest(html5TargetConfiguration))
-})
+        .pipe(exClient.dest(html5TargetConfiguration));
+});
 
-var otherPaths = [
+const otherPaths = [
     '*.html',
     '*.xql',
     'templates/**/*',
@@ -95,15 +93,15 @@ var otherPaths = [
 gulp.task('deploy:other', function () {
     return gulp.src(otherPaths, {base: './'})
         .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-})
+        .pipe(exClient.dest(targetConfiguration));
+});
 
-gulp.task('deploy', ['deploy:other', 'deploy:components', 'deploy:styles'])
+gulp.task('deploy', gulp.parallel('deploy:other', 'deploy:components', 'deploy:styles'));
 
 gulp.task('watch', function () {
-    gulp.watch('resources/css/!*', ['deploy:styles'])
-    gulp.watch(otherPaths, ['deploy:other'])
-    gulp.watch('*.html', ['deploy:components'])
-})
+    gulp.watch('resources/css/*', gulp.series('deploy:styles'));
+    gulp.watch(otherPaths, gulp.series('deploy:other'));
+    gulp.watch('*.html', gulp.series('deploy:components'));
+});
 
-gulp.task('default', ['watch'])
+gulp.task('default', gulp.series('watch'));
